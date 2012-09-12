8b664b5ca06eca6e4ca0b2b0c35f95dbcd2964d8
Matthew Cook <Matthew.W.Cook@gmail.com>
2012-09-12 18:15:54 -0400

    A few functions seem to not work out of the gate. Maybe it is just eclipse being weird, and maybe I should have used a branch, but I think this needs to be resolved in master before further work is done.

--- Ocelot/src/com/capstone/ocelot/Default.java
+++ Ocelot/src/com/capstone/ocelot/Default.java
@@ -18,6 +18,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+@SuppressWarnings("unused")
 public class Default extends FragmentActivity implements ActionBar.TabListener {
 
     /**
@@ -89,7 +90,9 @@ public class Default extends FragmentActivity implements ActionBar.TabListener {
         return super.onOptionsItemSelected(item);
     }
 
-
+    
+    // Must rework these functions, they seem to give the compiler headaches.
+    /*
     @Override
     public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
     }
@@ -103,6 +106,7 @@ public class Default extends FragmentActivity implements ActionBar.TabListener {
     @Override
     public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
     }
+    */
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to one of the primary
