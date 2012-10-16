e1fe9f65e0f7ed8bdc64e84096ce2c3b6e608c50
Jesse Whitworth (jazzywhit) <jessecwhitworth@gmail.com>
2012-10-16 19:03:01 -0400

    Drag and Drop. - Put in initial drag and drop - The same OnTouchListener will also play sounds. - Currently losing the object on drag. Need to implement an OnDragListener or some other method.

--- Ocelot/src/com/capstone/ocelot/Default.java
+++ Ocelot/src/com/capstone/ocelot/Default.java
@@ -1,14 +1,19 @@
 package com.capstone.ocelot;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import android.app.Activity;
+import android.content.ClipData;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.DragShadowBuilder;
+import android.view.View.OnTouchListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemLongClickListener;
@@ -38,21 +43,45 @@ public class Default extends Activity {
     	//getWindowManager().getDefaultDisplay().getMetrics(metrics);
     	//int height = metrics.heightPixels;
     	//int width = metrics.widthPixels;
-		
-	    gridview.setOnItemClickListener(new OnItemClickListener() {  //Display the description associated with the object.
-	        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-	        	Toast.makeText(parent.getContext(), "" + mSoundBoardItems.get(position).getDescription(), Toast.LENGTH_SHORT).show();
-	        }
-	    });
 	    
-	    gridview.setOnItemLongClickListener(new OnItemLongClickListener() {  //Play the sound associated with the object.
-			public boolean onItemLongClick(AdapterView<?> parent, View v, int position, long id) {
-	        	MediaPlayer mp = MediaPlayer.create(parent.getContext(), mSoundBoardItems.get(position).getSoundResourceId());
-	        	mp.start(); 
-	        	return true;
-			}
-		});
+//	    Iterator<SoundBoardItem> itrItems = mSoundBoardItems.iterator();
+//	    while(itrItems.hasNext()){
+//	    	itrItems.next()..setOnTouchListener(new MyTouchListener());
+//	    }
+	    
+	    //findViewById(R.id.gridview).setOnTouchListener(new MyTouchListener()); //It appears that this needs to be added to a view, not an object in the view.
+		
+//	    gridview.setOnItemClickListener(new OnItemClickListener() {  //Display the description associated with the object.
+//	        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+//	        	MediaPlayer mp = MediaPlayer.create(parent.getContext(), mSoundBoardItems.get(position).getSoundResourceId());
+//	        	mp.start();
+//	        	Toast.makeText(parent.getContext(), "" + mSoundBoardItems.get(position).getDescription(), Toast.LENGTH_SHORT).show();
+//	        }
+//	    });
+	    
+//	    gridview.setOnItemLongClickListener(new OnItemLongClickListener() {  //Play the sound associated with the object.
+//			public boolean onItemLongClick(AdapterView<?> parent, View v, int position, long id) {
+//	        	MediaPlayer mp = MediaPlayer.create(parent.getContext(), mSoundBoardItems.get(position).getSoundResourceId());
+//	        	mp.start(); 
+//	        	return true;
+//			}
+//		});
 	}
+	
+//	//Touch Listener to interact with the SoundBoardItems
+//	private final class MyTouchListener implements OnTouchListener {
+//	    public boolean onTouch(View view, MotionEvent motionEvent) {
+//	      if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+//	        ClipData data = ClipData.newPlainText("", "");
+//	        DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+//	        view.startDrag(data, shadowBuilder, view, 0);
+//	        //view.setVisibility(View.INVISIBLE);
+//	        return true;
+//	      } else {
+//	        return false;
+//	      }
+//	    }
+//	}
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
--- Ocelot/src/com/capstone/ocelot/ImageAdapter.java
+++ Ocelot/src/com/capstone/ocelot/ImageAdapter.java
@@ -2,12 +2,21 @@ package com.capstone.ocelot;
 
 import java.util.ArrayList;
 
+import android.content.ClipData;
 import android.content.Context;
+import android.media.MediaPlayer;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.DragShadowBuilder;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class ImageAdapter extends BaseAdapter {
     private Context mContext;
@@ -58,7 +67,7 @@ public class ImageAdapter extends BaseAdapter {
     }
 
     // create a new ImageView for each item referenced by the Adapter
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         	
         ImageView imageView;
         if (convertView == null) {  // if it's not recycled, initialize some attributes
@@ -71,6 +80,110 @@ public class ImageAdapter extends BaseAdapter {
         }
 
         imageView.setImageResource(mSoundBoardItems.get(position).getIconResourceId());
+        imageView.setOnTouchListener(new OnTouchListener(){
+        	
+			public boolean onTouch(View view, MotionEvent motionEvent) {
+				if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+					MediaPlayer mp = MediaPlayer.create(mContext, mSoundBoardItems.get(position).getSoundResourceId());
+		        	mp.start();
+		        	return true;
+				} else if (motionEvent.getAction() == MotionEvent.ACTION_MOVE){
+					ClipData data = ClipData.newPlainText("", "");
+					DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+					view.startDrag(data, shadowBuilder, view, 0);
+					view.setVisibility(View.INVISIBLE);
+					return true;
+				} else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+					view.setVisibility(View.VISIBLE);
+		        	return true;
+				} else {
+					//view.setVisibility(View.VISIBLE);
+					return false;
+			  }
+			}
+        });
+      
+//	    imageView.setOnClickListener(new OnClickListener() {  //Play the sound associated with the object.
+//			public void onClick(View v) {
+//				// TODO Auto-generated method stub
+//				MediaPlayer mp = MediaPlayer.create(mContext, mSoundBoardItems.get(position).getSoundResourceId());
+//	        	mp.start();
+//			}
+//		});
         return imageView;
     }
+    
+	//Touch Listener to interact with the SoundBoardItems
+	private class MyTouchListener implements OnTouchListener {
+	    public boolean onTouch(View view, MotionEvent motionEvent) {
+	      if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+	        ClipData data = ClipData.newPlainText("", "");
+	        DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+	        view.startDrag(data, shadowBuilder, view, 0);
+	        //view.setVisibility(View.INVISIBLE);
+	        return true;
+	      } else {
+	        return false;
+	      }
+	    }
+	}
+	
+//	private class MyTouchListener implements OnTouchListener {
+//	    public boolean onTouch(View v, MotionEvent event) {
+//	        // TODO Auto-generated method stub
+//	
+//	        if(event.getAction() == MotionEvent.ACTION_DOWN)
+//	        {
+//	            v.setBackgroundResource(R.drawable.chicken); //Will need to create a drop shadow for this.
+//	            Log.v("log_tag", "MotionEvent.ACTION_DOWN");
+//	            MediaPlayer mp = MediaPlayer.create(mContext, mSoundBoardItems.get(position).getSoundResourceId());
+//	            mp.start();
+//	            return true;
+//	        }
+//	        else if(event.getAction()==MotionEvent.ACTION_UP)
+//	        {
+//	
+//	            //v.setBackgroundDrawable(null); 
+//	            Log.v("log_tag", "MotionEvent.ACTION_UP");
+//	            return true; 
+//	        }
+//	        return false;
+//	    }
+//	}
+    
+ // create a new ImageView for each item referenced by the Adapter
+//    public View getView(int position, View convertView, ViewGroup parent) {
+//        ImageView imageView;
+//        if (convertView == null) {  // if it's not recycled, initialize some attributes
+//            imageView = new ImageView(mContext);
+//            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+//            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+//            imageView.setPadding(8, 8, 8, 8);
+//        } else {
+//            imageView = (ImageView) convertView;
+//        }
+//
+//        imageView.setImageResource(mSoundBoardItems.get([position));
+//        imageView.setOnTouchListener(new OnTouchListener() {
+//
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                // TODO Auto-generated method stub
+//
+//                if(event.getAction() == MotionEvent.ACTION_DOWN)
+//                {
+//                    v.setBackgroundResource(R.drawable.sample_0);
+//                    Log.v("log_tag", "MotionEvent.ACTION_DOWN");
+//                    return true;
+//                }
+//                else if(event.getAction()==MotionEvent.ACTION_UP)
+//                {
+//                    Log.v("log_tag", "MotionEvent.ACTION_UP");
+//                    return true; 
+//                }
+//                return false;
+//            }
+//        });
+//        return imageView;
+//    }
 }
